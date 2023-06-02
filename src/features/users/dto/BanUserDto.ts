5bf15e80f4cc029cf48d934d4d01e62e77863359
Maksym <qlowlight@gmail.com>
2023-06-02 11:02:23 +0300

    change user dto for tests

--- src/features/users/dto/BanUserDto.ts
+++ src/features/users/dto/BanUserDto.ts
@@ -1,12 +1,10 @@
-import { IsBoolean, IsNotEmpty, IsString, MaxLength } from 'class-validator';
-
 export class BanUserDto {
-  @IsNotEmpty()
-  @IsBoolean()
+  // @IsNotEmpty()
+  // @IsBoolean()
   isBanned: boolean;
 
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(20)
+  // @IsNotEmpty()
+  // @IsString()
+  // @MaxLength(20)
   banReason: string;
 }
