68979c91f9f274f6af5a3a0f9e7016c4b7e1841f
Maksym <qlowlight@gmail.com>
2023-06-02 11:46:07 +0300

    remove all redundant methods from public posts endpoint

--- src/features/posts/application/posts.public.service.ts
+++ src/features/posts/application/posts.public.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PostsRepository } from '../repository/posts.repository';
 import { BlogsRepository } from '../../blogs/repository/blogs.repository';
-import { CreatePostInputDto } from '../dto/CreatePostInputDto';
-import { UpdatePostInputDto } from '../dto/UpdatePostInputDto';
-import { CustomResponse } from '../../utils/customResponse/CustomResponse';
-import { CustomResponseEnum } from '../../utils/customResponse/CustomResponseEnum';
-import { PostDocument } from '../schemas/post.schema';
 import { LikesService } from '../../likes/likes.service';
 
 @Injectable()
@@ -16,83 +11,24 @@ export class PostsPublicService {
     private readonly likeService: LikesService,
   ) {}
 
-  // async createPost(createPostInputData: CreatePostInputDto) {
-  //   const createPostDto: CreatePostInputDto | null = await this.createDto(
-  //     createPostInputData,
-  //   );
-  //
-  //   if (!createPostDto) return null;
-  //
-  //   return await this.postRepository.createPost(createPostDto);
-  // }
-  //
-  // async updatePost(
-  //   updatePostInputData: UpdatePostInputDto,
-  //   postId: string,
-  // ): Promise<CustomResponse<null>> {
-  //   //get post
-  //   const post: PostDocument | null = await this.postRepository.getPostById(
-  //     postId,
-  //   );
-  //
-  //   if (!post) return new CustomResponse(false, CustomResponseEnum.notExist);
-  //   //if blog id invalid return null
-  //   const updatePostDto: UpdatePostInputDto | null = await this.createDto(
-  //     updatePostInputData,
-  //   );
-  //
-  //   if (!updatePostDto)
-  //     return new CustomResponse(false, CustomResponseEnum.badRequest);
-  //
-  //   post.updateData(updatePostDto);
-  //
-  //   await this.postRepository.save(post);
-  //
-  //   return new CustomResponse(true);
-  // }
-  //
-  // async createDto(dto: CreatePostInputDto | UpdatePostInputDto) {
-  //   //in blog exist, get blog name
-  //   const blogName = await this.blogRepository.getBlogNameById(dto.blogId);
-  //
-  //   if (!blogName) return null;
-  //
-  //   //create correct dto with blog name and return
-  //   return {
-  //     title: dto.title,
-  //     shortDescription: dto.shortDescription,
-  //     content: dto.content,
-  //     blogId: dto.blogId,
-  //     blogName: blogName,
-  //   };
-  // }
-  //
-  // async setLikeStatus(
-  //   likeStatus: string,
-  //   targetId: string,
-  //   userId: string,
-  //   userLogin: string,
-  // ): Promise<boolean> {
-  //   const isPostExist = await this.postRepository.isPostExist(targetId);
-  //   if (!isPostExist) return false;
-  //
-  //   await this.likeService.setLikeStatus(
-  //     { likeStatus, targetId, userId, userLogin },
-  //     'post',
-  //   );
-  //
-  //   return true;
-  // }
-  //
+  async setLikeStatus(
+    likeStatus: string,
+    targetId: string,
+    userId: string,
+    userLogin: string,
+  ): Promise<boolean> {
+    const isPostExist = await this.postRepository.isPostExist(targetId);
+    if (!isPostExist) return false;
+
+    await this.likeService.setLikeStatus(
+      { likeStatus, targetId, userId, userLogin },
+      'post',
+    );
+
+    return true;
+  }
+
   async checkIsPostExistById(id: string) {
     return await this.postRepository.isPostExist(id);
   }
-  //
-  // async deletePost(id: string) {
-  //   const isPostExist: boolean = await this.postRepository.isPostExist(id);
-  //
-  //   if (!isPostExist) return false;
-  //
-  //   return this.postRepository.deletePost(id);
-  // }
 }
--- src/features/posts/posts.controller.ts
+++ src/features/posts/posts.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   HttpCode,
   Param,
@@ -16,9 +15,6 @@ import {
 } from '../utils/query-mappers/post-query-mapper';
 import { PostsQueryRepository } from './repository/posts.query.repository';
 import { PostsPublicService } from './application/posts.public.service';
-import { CreatePostInputDto } from './dto/CreatePostInputDto';
-import { UpdatePostInputDto } from './dto/UpdatePostInputDto';
-import { CustomResponse } from '../utils/customResponse/CustomResponse';
 import { CustomResponseEnum } from '../utils/customResponse/CustomResponseEnum';
 import {
   CommentInputQueryType,
@@ -31,7 +27,6 @@ import { CommentsService } from '../comments/comments.service';
 import { CurrentUser } from '../common/decorators/current.user.decorator';
 import { LikeInputDto } from '../likes/dto/LikeInputDto';
 import { OptionalJwtAuthGuard } from '../auth/guards/optional.jwt.guard';
-import { BasicAuthGuard } from '../auth/guards/basic.auth.guard';
 import { Exceptions } from '../utils/throwException';
 
 @Controller('posts')
@@ -43,117 +38,86 @@ export class PostsController {
     private readonly commentsService: CommentsService,
   ) {}
 
-  // @Get()
-  // @UseGuards(OptionalJwtAuthGuard)
-  // async getBlogs(@Query() query: PostInputQueryType, @CurrentUser() user) {
-  //   const postQuery = postQueryMapper(query);
-  //   const currentUserId = user?.id || null;
-  //
-  //   return await this.postQueryRepository.getPosts(postQuery, currentUserId);
-  // }
-  //
-  // @Get(':id')
-  // @UseGuards(OptionalJwtAuthGuard)
-  // async getPostById(@Param('id') id: string, @CurrentUser() user) {
-  //   const currentUserId = user?.id || null;
-  //   const post = await this.postQueryRepository.getPostById(id, currentUserId);
-  //   if (!post)
-  //     return Exceptions.throwHttpException(CustomResponseEnum.notExist);
-  //   return post;
-  // }
-  // @Post()
-  // @UseGuards(BasicAuthGuard)
-  // @HttpCode(201)
-  // async createPost(@Body() dto: CreatePostInputDto) {
-  //   return await this.postService.createPost(dto);
-  // }
-  //
-  // @Put(':id')
-  // @UseGuards(BasicAuthGuard)
-  // @HttpCode(204)
-  // async updatePost(@Body() dto: UpdatePostInputDto, @Param('id') id: string) {
-  //   const result: CustomResponse<null> = await this.postService.updatePost(
-  //     dto,
-  //     id,
-  //   );
-  //
-  //   if (!result.isSuccess)
-  //     return Exceptions.throwHttpException(result.errStatusCode);
-  //
-  //   return;
-  // }
-  //
-  // @Delete(':id')
-  // @UseGuards(BasicAuthGuard)
-  // @HttpCode(204)
-  // async deletePost(@Param('id') id: string) {
-  //   const deleteResult = await this.postService.deletePost(id);
-  //   if (!deleteResult)
-  //     return Exceptions.throwHttpException(CustomResponseEnum.notExist);
-  //   return;
-  // }
-  //
-  // @Get(':id/comments')
-  // @UseGuards(OptionalJwtAuthGuard)
-  // async getPostComments(
-  //   @Param('id') id: string,
-  //   @Query() inputQuery: CommentInputQueryType,
-  //   @CurrentUser() user,
-  // ) {
-  //   const commentQuery = commentQueryMapper(inputQuery);
-  //   const currentUserId = user?.id || null;
-  //
-  //   const isPostExist = await this.postService.checkIsPostExistById(id);
-  //   if (!isPostExist)
-  //     return Exceptions.throwHttpException(CustomResponseEnum.notExist);
-  //
-  //   return await this.commentQueryRepository.getPostCommentsWithPaginator(
-  //     commentQuery,
-  //     id,
-  //     currentUserId,
-  //   );
-  // }
-  //
-  // @UseGuards(JwtAuthGuard)
-  // @Post(':id/comments')
-  // async createCommentForPost(
-  //   @Param('id') id: string,
-  //   @Body() dto: CommentDto,
-  //   @CurrentUser() user: { id: string; userName: string },
-  // ) {
-  //   const createdCommentId: string | null =
-  //     await this.commentsService.createComment(
-  //       dto.content,
-  //       id,
-  //       user.id,
-  //       user.userName,
-  //     );
-  //   if (!createdCommentId)
-  //     Exceptions.throwHttpException(CustomResponseEnum.notExist);
-  //   const comment = await this.commentQueryRepository.getCommentById(
-  //     createdCommentId,
-  //     null,
-  //   );
-  //   if (!comment) Exceptions.throwHttpException(CustomResponseEnum.notExist);
-  //   return comment;
-  // }
-  // @UseGuards(JwtAuthGuard)
-  // @HttpCode(204)
-  // @Put(':id/like-status')
-  // async setLikeStatus(
-  //   @Param('id') id: string,
-  //   @Body() dto: LikeInputDto,
-  //   @CurrentUser() user: { id: string; userName: string },
-  // ) {
-  //   const isLikeSet = await this.postService.setLikeStatus(
-  //     dto.likeStatus,
-  //     id,
-  //     user.id,
-  //     user.userName,
-  //   );
-  //
-  //   if (!isLikeSet) Exceptions.throwHttpException(CustomResponseEnum.notExist);
-  //
-  //   return;
-  // }
+  @Get()
+  @UseGuards(OptionalJwtAuthGuard)
+  async getBlogs(@Query() query: PostInputQueryType, @CurrentUser() user) {
+    const postQuery = postQueryMapper(query);
+    const currentUserId = user?.id || null;
+
+    return await this.postQueryRepository.getPosts(postQuery, currentUserId);
+  }
+
+  @Get(':id')
+  @UseGuards(OptionalJwtAuthGuard)
+  async getPostById(@Param('id') id: string, @CurrentUser() user) {
+    const currentUserId = user?.id || null;
+    const post = await this.postQueryRepository.getPostById(id, currentUserId);
+    if (!post)
+      return Exceptions.throwHttpException(CustomResponseEnum.notExist);
+    return post;
+  }
+
+  @Get(':id/comments')
+  @UseGuards(OptionalJwtAuthGuard)
+  async getPostComments(
+    @Param('id') id: string,
+    @Query() inputQuery: CommentInputQueryType,
+    @CurrentUser() user,
+  ) {
+    const commentQuery = commentQueryMapper(inputQuery);
+    const currentUserId = user?.id || null;
+
+    const isPostExist = await this.postService.checkIsPostExistById(id);
+    if (!isPostExist)
+      return Exceptions.throwHttpException(CustomResponseEnum.notExist);
+
+    return await this.commentQueryRepository.getPostCommentsWithPaginator(
+      commentQuery,
+      id,
+      currentUserId,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':id/comments')
+  async createCommentForPost(
+    @Param('id') id: string,
+    @Body() dto: CommentDto,
+    @CurrentUser() user: { id: string; userName: string },
+  ) {
+    const createdCommentId: string | null =
+      await this.commentsService.createComment(
+        dto.content,
+        id,
+        user.id,
+        user.userName,
+      );
+    if (!createdCommentId)
+      Exceptions.throwHttpException(CustomResponseEnum.notExist);
+    const comment = await this.commentQueryRepository.getCommentById(
+      createdCommentId,
+      null,
+    );
+    if (!comment) Exceptions.throwHttpException(CustomResponseEnum.notExist);
+    return comment;
+  }
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(204)
+  @Put(':id/like-status')
+  async setLikeStatus(
+    @Param('id') id: string,
+    @Body() dto: LikeInputDto,
+    @CurrentUser() user: { id: string; userName: string },
+  ) {
+    const isLikeSet = await this.postService.setLikeStatus(
+      dto.likeStatus,
+      id,
+      user.id,
+      user.userName,
+    );
+
+    if (!isLikeSet) Exceptions.throwHttpException(CustomResponseEnum.notExist);
+
+    return;
+  }
 }
