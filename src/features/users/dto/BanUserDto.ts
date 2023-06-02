868ee35ad8b726e821969b903a95de4d805d7acf
Maksym <qlowlight@gmail.com>
2023-06-02 15:12:39 +0300

    fix dto validation incorrect value

--- src/features/users/dto/BanUserDto.ts
+++ src/features/users/dto/BanUserDto.ts
@@ -1,8 +1,11 @@
+import { IsBoolean, IsNotEmpty, MinLength } from 'class-validator';
+
 export class BanUserDto {
-  // @IsNotEmpty()
+  @IsNotEmpty()
+  @IsBoolean()
   isBanned: boolean;
 
-  // @IsNotEmpty()
-  // @MaxLength(20)
+  @IsNotEmpty()
+  @MinLength(20)
   banReason: string;
 }
